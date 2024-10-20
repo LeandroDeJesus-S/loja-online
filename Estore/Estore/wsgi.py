cc7af669cc8dfbf19eef89de7a2a94f017940d42
LeandroDeJesus-S <jstleandro@gmail.com>
2024-10-20 13:18:09 -0300

    feat: delete dotev_files dir and change dockerfile and docker-compose files

--- Estore/Estore/wsgi.py
+++ Estore/Estore/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR.parent / '.env', override=True)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Estore.settings')
 
--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..."
-  sleep 2
-done
-
 echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
 
 install_dev_dependencies.sh
